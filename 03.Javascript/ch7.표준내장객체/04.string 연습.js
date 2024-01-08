9d1b386bd32ea31b547878244dc50ec88c7a392f
yeoungjunyoon <dudwnsv123@naver.com>
2024-01-08 15:35:50 +0900

    표준내장객체.js

---
+++ 03.Javascript/ch7.표준내장객체/04.string 연습.js
@@ -0,0 +1,30 @@
+    // 연습문제
+    // 1. 1에서 1000사이에 0은 몇번, 1은 몇번, ..., 9는 몇번 사용되었는가?
+    //      123456789101112131415...9991000
+    let numStr = '';
+    for (let i = 1; i <= 1000; i++) {
+        numStr += i;
+    }
+    // 1이 몇번 사용되었나?
+    console.log(numStr.replace(/[^1]/g,'').length);
+
+
+    let pattern = new RegExp('[^1]', 'g');
+    console.log(numStr.replace(pattern, '').length);
+
+    for (let i = 0; i <= 9; i++) {
+        let pattern = new RegExp('[^' + i + ']', 'g');
+        count = numStr.replace(pattern, '').length;
+        console.log(`${i}: ${count}`);
+    }
+
+    // count 배열을 만들어 일일이 체크
+    let countArr = [0,0,0,0,0,0,0,0,0,0,]
+    for (let i = 0; i < numStr.length; i++) {
+        let num = Number(numStr[i]);
+        countArr[num]++;
+    }
+    console.log(countArr);
+    
+
+
