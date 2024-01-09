47518e8c07d69260b5a499d3aff2d24e2bc07b30
yeoungjunyoon <dudwnsv123@naver.com>
2024-01-10 08:56:20 +0900

    Add files via upload

---
+++ 03.Javascript/ch07.표준내장객체/04.string 연습.js
@@ -0,0 +1,79 @@
+     // 연습문제
+    // 1. 1에서 1000사이에 0은 몇번, 1은 몇번, ..., 9는 몇번 사용되었는가?
+    //      123456789101112131415...9991000
+    let numStr = '';
+    for (let i = 1; i <= 1000; i++) {
+        numStr += i;
+    }
+
+    
+    // 정규표현식 활용
+    // 1이 몇번 사용되었나?
+    console.log(numStr.replace(/[^1]/g,'').length);
+
+
+    let pattern = new RegExp('[^1]', 'g');
+    console.log(numStr.replace(pattern, '').length);
+
+    for (let i = 0; i <= 9; i++) {
+        let pattern = new RegExp('[^' + i + ']', 'g');
+        count = numStr.replace(pattern, '').length;
+        console.log(`${i}: ${count}`);
+    }
+
+    // count 배열을 만들어 일일이 체크
+    let countArr = [0,0,0,0,0,0,0,0,0,0,]
+    for (let i = 0; i < numStr.length; i++) {
+        let num = Number(numStr[i]);
+        countArr[num]++;
+    }
+    console.log(countArr); 
+
+
+    // 2. 디지털 시계, 00:00 ~ 23:59
+    // 하루동안 3이 표시되는 시간은 몇초인가?
+    let displayTime = 0;
+    for (let hour = 0; hour <=23; hoyr++) {
+        for (let minute = 0; minute <= 59; minute) {
+            let clock = hour + ':' +minute;
+            if (clock,indexOf('3') >= 0)
+            displayTime += 60;
+        }
+    }
+    console.log(`하루동안 3이 표시되는 시간은 ${displayTime}초 입니다.`);
+
+    // 3. 두개의 세자리수를 곱해서 나온 결과가 palindrome일때
+    // 가장 큰 palindrome 수와 어떤 수를 곱해서 나온 결과인가?
+    function ispalindrome(s) {
+        return s == s.split ('').reverse().join('');  
+    }
+
+    let maxPal = 0, max1 = 0, max2 = 0;
+    for (let i = 100; i <= 999; i++) {
+        for (let k = 100; k <= 999; k ++) {
+            let mul = i * k;
+            if (ispalindrome(String(mul))) {
+                if (mul > maxPal) {
+                    maxPal = mul; max1 = i; max2 = k;
+            }
+        }
+    }
+ }
+ console.log(`${max1} x ${max2} = ${maxpal}`);
+
+    // 4. c:/workspace/webproject/03.JavaScript/ch07.표준내장객체/04.string연습.js
+    // 에서 파일명(04.String연습.js)만 출력하세요.
+
+    // split 후 맨 마지막 요소
+    let pathArr = path.split('/');
+    console.log(pathArr[pathArr.length - 1]);
+
+    // 맨 마지막에 있는 / 위치를 찾아서 substring 메소드를 찾는 방법
+    let fileIndex = path.lastIndexOf('/');
+    console.log(path.substring(fileIndex + 1));
+
+
+    
+    
+
+
